2b8b344d5c6f837dcf0627853f9c9d3259a4a661
Esau Orozco <esau.orozco.r@gmail.com>
2022-09-22 08:21:11 -0500

    Format files

--- nvim/.config/nvim/lua/user/lspsaga.lua
+++ nvim/.config/nvim/lua/user/lspsaga.lua
@@ -1,20 +1,20 @@
-  local saga_status_ok, lspSaga = pcall(require, "lspsaga")
-  if not saga_status_ok then
-    return
-  end
+local saga_status_ok, lspSaga = pcall(require, "lspsaga")
+if not saga_status_ok then
+	return
+end
 
 lspSaga.init_lsp_saga({
-    border_style = 'rounded',
-    saga_winblend = 30
-  })
+	border_style = "rounded",
+	saga_winblend = 30,
+})
 
-local opts = {noremap = true, silent = true}
-vim.keymap.set('n', '<A-j>', '<cmd>Lspsaga diagnostic_jump_next<CR>', opts)
-vim.keymap.set('n', '<A-k>', '<cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
+local opts = { noremap = true, silent = true }
+vim.keymap.set("n", "<A-j>", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+vim.keymap.set("n", "<A-k>", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
 
-vim.keymap.set('n', '<leader>ca', '<cmd>Lspsaga code_action<cr>', opts)
-vim.keymap.set('n', 'K', '<cmd>Lspsaga hover_doc<cr>', opts)
-vim.keymap.set('n', 'gh', '<cmd>Lspsaga lsp_finder<cr>', opts)
-vim.keymap.set('n', '<C-k>', '<cmd>Lspsaga signature_help<cr>', opts)
-vim.keymap.set('n', 'gd', '<cmd>Lspsaga preview_definition<cr>', opts)
-vim.keymap.set('n', 'gr', '<cmd>Lspsaga rename<cr>', opts)
+vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<cr>", opts)
+vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<cr>", opts)
+vim.keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<cr>", opts)
+vim.keymap.set("n", "<C-k>", "<cmd>Lspsaga signature_help<cr>", opts)
+vim.keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<cr>", opts)
+vim.keymap.set("n", "gr", "<cmd>Lspsaga rename<cr>", opts)
--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -1,99 +1,107 @@
 -- Automatically install packer if not present --
 local fn = vim.fn
-local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    'git',
-    'clone',
-    '--depth',
-    '1',
-    'https://github.com/wbthomason/packer.nvim',
-    install_path,
-  }
-  print 'Installing packer close and reopen Neovim...'
-  vim.cmd [[packadd packer.nvim]]
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
 end
 
-local status_ok, packer = pcall(require, 'packer')
+local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+	return
 end
 ------------------------------------------------
 
 -- Autocammand to install/updates packages after file has been saved --
-vim.cmd [[
+vim.cmd([[
   augroup packer_user_config
     autocmd!
     autocmd BufWritePost plugins.lua source <afile> | PackerSync
   augroup end
-]]
+]])
 -----------------------------------------------------------------------
 
--- Packer configuration options -- 
-packer.init {
-  display = {
-    open_fn = function()
-      return require('packer.util').float { border = 'rounded' }
-    end,
-  },
-}
+-- Packer configuration options --
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
 ----------------------------------
 
 return packer.startup(function()
+	use("wbthomason/packer.nvim")
+	use("nvim-lua/plenary.nvim")
+	use({ "kyazdani42/nvim-tree.lua", requires = { "kyazdani42/nvim-web-devicons" } })
+	use("windwp/nvim-autopairs")
+	use({ "nvim-lualine/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons", opt = true } })
+	use("numToStr/Comment.nvim")
+	use({ "akinsho/toggleterm.nvim", tag = "v2.*" })
+	use({ "MunifTanjim/prettier.nvim", requires = { "neovim/nvim-lspconfig", "jose-elias-alvarez/null-ls.nvim" } })
+	use({ "akinsho/bufferline.nvim", tag = "v2.*", requires = { "kyazdani42/nvim-web-devicons" } })
+	use({
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup()
+		end,
+	})
 
-  use 'wbthomason/packer.nvim'
-  use 'nvim-lua/plenary.nvim'
-  use { 'kyazdani42/nvim-tree.lua', requires = {'kyazdani42/nvim-web-devicons'} }
-  use 'windwp/nvim-autopairs'
-  use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
-  use 'numToStr/Comment.nvim'
-  use { 'akinsho/toggleterm.nvim', tag = 'v2.*' }
-  use { 'MunifTanjim/prettier.nvim', requires = { 'neovim/nvim-lspconfig', 'jose-elias-alvarez/null-ls.nvim'}}
-  use { 'akinsho/bufferline.nvim', tag = 'v2.*', requires = { 'kyazdani42/nvim-web-devicons'}}
-  use {'norcalli/nvim-colorizer.lua', config = function() require('colorizer').setup() end }
+	use("junegunn/fzf.vim")
+	use({
+		"junegunn/fzf",
+		run = function()
+			vim.fn["fzf#install"]()
+		end,
+	})
 
-  use 'junegunn/fzf.vim'
-  use {'junegunn/fzf',
-    run = function()
-      vim.fn['fzf#install']()
-    end
-  }
+	-- Colorschemes
+	use("sainnhe/gruvbox-material")
+	use("arcticicestudio/nord-vim")
+	use("EdenEast/nightfox.nvim")
 
-  -- Colorschemes
-  use 'sainnhe/gruvbox-material'
-  use 'arcticicestudio/nord-vim'
-  use 'EdenEast/nightfox.nvim'
+	-- LSP
+	use("neovim/nvim-lspconfig")
+	use("williamboman/nvim-lsp-installer")
+	use("jose-elias-alvarez/null-ls.nvim")
+	use({ "glepnir/lspsaga.nvim", branch = "main" })
 
-  -- LSP
-  use 'neovim/nvim-lspconfig'
-  use 'williamboman/nvim-lsp-installer'
-  use 'jose-elias-alvarez/null-ls.nvim'
-  use { 'glepnir/lspsaga.nvim', branch = 'main' }
+	-- CMP
+	use("hrsh7th/nvim-cmp") -- Engine
+	use("L3MON4D3/LuaSnip") -- Snippet engine
+	use("saadparwaiz1/cmp_luasnip") -- snippet completions
+	use("hrsh7th/cmp-nvim-lsp") -- LSP capabilities
+	use("hrsh7th/cmp-path") -- Path completions
+	use("hrsh7th/cmp-buffer") -- buffer values completions
+	use("hrsh7th/vscode-langservers-extracted")
+	use("rafamadriz/friendly-snippets")
 
-  -- CMP
-  use 'hrsh7th/nvim-cmp' -- Engine
-  use 'L3MON4D3/LuaSnip' -- Snippet engine
-  use 'saadparwaiz1/cmp_luasnip' -- snippet completions
-  use 'hrsh7th/cmp-nvim-lsp' -- LSP capabilities
-  use 'hrsh7th/cmp-path' -- Path completions
-  use 'hrsh7th/cmp-buffer' -- buffer values completions
+	-- Telescope
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = {
+			{ "kyazdani42/nvim-web-devicons" },
+			{ "nvim-lua/plenary.nvim" },
+			{ "nvim-telescope/telescope-file-browser.nvim" },
+			{ "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
+		},
+	})
 
-  -- Telescope
-  use {'nvim-telescope/telescope.nvim', 
-    requires = {
-      { 'kyazdani42/nvim-web-devicons' },
-      { 'nvim-lua/plenary.nvim' },
-      { 'nvim-telescope/telescope-file-browser.nvim' },
-      { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
-    }
-  }
+	-- Treesitter
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	use("p00f/nvim-ts-rainbow")
+	use("windwp/nvim-ts-autotag")
 
-  -- Treesitter
-  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
-  use 'p00f/nvim-ts-rainbow'
-  use 'windwp/nvim-ts-autotag'
-
-  if PACKER_BOOTSTRAP then
-    require('packer').sync()
-  end
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)
